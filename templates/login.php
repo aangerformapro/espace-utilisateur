8efd25622feb367751f2529362077389dc16f8a9
Aymeric Anger <a.anger@codeur.online>
2023-08-02 08:55:54 +0200

    required fields

--- templates/login.php
+++ templates/login.php
@@ -15,11 +15,11 @@ include 'page/header.php';
     <div class="card-text">
       <form method="post" action="./login.php">
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+          <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
           <label for="username">Nom d'utilisateur</label>
         </div>
         <div class="form-floating mb-3">
-          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+          <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
           <label for="password">Mot de passe</label>
         </div>
 
